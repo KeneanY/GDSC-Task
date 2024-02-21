8f81083a98a8c764db94433c70609f9c024fa1f1
Yabesera-Y <Keneanyilma6@gmail.com>
2024-02-21 20:33:10 +0300

    Initial commit

---
+++ Month-2/week-2/ShoppingCart/src/App.jsx
@@ -0,0 +1,13 @@
+
+import ShoppingCart from './ShoppingCart';
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Online Store</h1>
+      <ShoppingCart />
+    </div>
+  );
+};
+
+export default App;
